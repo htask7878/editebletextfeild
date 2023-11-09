5cbf551945249a2eda2b23c6dee7217ee2db4db4
H T Beladiya <pdfile005@gmail.com>
2023-11-09 23:16:10 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/editebletextfeild/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.editebletextfeild
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,169 @@
+import 'dart:convert';
+
+import 'package:fleather/fleather.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quill_delta/quill_delta.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const FleatherApp());
+}
+
+class FleatherApp extends StatelessWidget {
+  const FleatherApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.light(),
+    home: HomePage(),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final FocusNode _focusNode = FocusNode();
+  FleatherController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _initController();
+  }
+
+  Future<void> _initController() async {
+    try {
+      final result = await rootBundle.loadString('assets/welcome.json');
+      final heuristics = ParchmentHeuristics(
+        formatRules: [],
+        insertRules: [
+          ForceNewlineForInsertsAroundInlineImageRule(),
+        ],
+        deleteRules: [],
+      ).merge(ParchmentHeuristics.fallback);
+      final doc = ParchmentDocument.fromJson(
+        jsonDecode(result),
+        heuristics: heuristics,
+      );
+      _controller = FleatherController(doc);
+    } catch (err, st) {
+      print('Cannot read welcome.json: $err\n$st');
+      _controller = FleatherController();
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(elevation: 0, title: Text('Fleather Demo')),
+      body: _controller == null
+          ? Center(child: const CircularProgressIndicator())
+          : Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: [
+          FleatherToolbar.basic(controller: _controller!),
+          Divider(height: 1, thickness: 0.5, color: Colors.grey.shade200),
+          SizedBox(height: 10,),
+          Container(
+            color: Colors.white,
+            child: FleatherEditor(
+              controller: _controller!,
+              focusNode: _focusNode,
+              showCursor: true,
+              padding: EdgeInsets.only(
+                left: 16,
+                right: 16,
+                bottom: MediaQuery.of(context).padding.bottom,
+              ),
+              onLaunchUrl: _launchUrl,
+              maxContentWidth: 800,
+              embedBuilder: _embedBuilder,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _embedBuilder(BuildContext context, EmbedNode node) {
+    if (node.value.type == 'hr') {
+      final theme = FleatherTheme.of(context)!;
+      return Divider(
+        height: theme.paragraph.style.fontSize! * theme.paragraph.style.height!,
+        thickness: 2,
+        color: Colors.grey.shade200,
+      );
+    }
+
+    if (node.value.type == 'icon') {
+      final data = node.value.data;
+      // Icons.rocket_launch_outlined
+      return SizedBox();
+    }
+
+    if (node.value.type == 'image' &&
+        node.value.data['source_type'] == 'assets') {
+      return SizedBox();
+    }
+
+    throw UnimplementedError();
+  }
+
+  void _launchUrl(String? url) async {
+    if (url == null) return;
+    final uri = Uri.parse(url);
+    final _canLaunch = await canLaunchUrl(uri);
+    if (_canLaunch) {
+      await launchUrl(uri);
+    }
+  }
+}
+
+/// This is an example insert rule that will insert a new line before and
+/// after inline image embed.
+class ForceNewlineForInsertsAroundInlineImageRule extends InsertRule {
+  @override
+  Delta? apply(Delta document, int index, Object data) {
+    if (data is! String) return null;
+
+    final iter = DeltaIterator(document);
+    final previous = iter.skip(index);
+    final target = iter.next();
+    final cursorBeforeInlineEmbed = _isInlineImage(target.data);
+    final cursorAfterInlineEmbed =
+        previous != null && _isInlineImage(previous.data);
+
+    if (cursorBeforeInlineEmbed || cursorAfterInlineEmbed) {
+      final delta = Delta()..retain(index);
+      if (cursorAfterInlineEmbed && !data.startsWith('\n')) {
+        delta.insert('\n');
+      }
+      delta.insert(data);
+      if (cursorBeforeInlineEmbed && !data.endsWith('\n')) {
+        delta.insert('\n');
+      }
+      return delta;
+    }
+    return null;
+  }
+
+  bool _isInlineImage(Object data) {
+    if (data is EmbeddableObject) {
+      return data.type == 'image' && data.inline;
+    }
+    if (data is Map) {
+      return data[EmbeddableObject.kTypeKey] == 'image' &&
+          data[EmbeddableObject.kInlineKey];
+    }
+    return false;
+  }
+}
